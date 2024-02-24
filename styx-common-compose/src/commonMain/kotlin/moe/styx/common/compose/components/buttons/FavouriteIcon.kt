c7079a7217da4078f75d07de60f3e14923ce59da
Alex H <48185123+Vodes@users.noreply.github.com>
2024-02-24 16:56:22 +0100

    Reimplement FavIconButton behavior

--- styx-common-compose/src/commonMain/kotlin/moe/styx/common/compose/components/buttons/FavouriteIcon.kt
+++ styx-common-compose/src/commonMain/kotlin/moe/styx/common/compose/components/buttons/FavouriteIcon.kt
@@ -7,36 +7,19 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import kotlinx.coroutines.runBlocking
 import moe.styx.common.compose.extensions.isFav
-import moe.styx.common.compose.files.Storage
-import moe.styx.common.compose.files.updateList
-import moe.styx.common.compose.http.login
-import moe.styx.common.data.Favourite
+import moe.styx.common.compose.threads.RequestQueue
 import moe.styx.common.data.Media
-import moe.styx.common.extension.currentUnixSeconds
-import moe.styx.common.extension.eqI
 
 @Composable
 fun FavouriteIconButton(media: Media, modifier: Modifier = Modifier) {
     var isFav by remember { mutableStateOf(media.isFav()) }
     IconButton({
-        //TODO: Implement with new request queue
-//        if (!isFav)
-//            RequestQueue.addFav(media)
-//        else
-//            RequestQueue.removeFav(media)
-//        isFav = media.isFav()
-//        favsTab.searchState.value = favsTab.mediaSearch.getDefault(updateList = DataManager.media.value.filter { it.isFav() })
-        runBlocking {
-            Storage.stores.favouriteStore.updateList { list ->
-                if (isFav)
-                    list.removeAll { it.mediaID eqI media.GUID }
-                else
-                    list.add(Favourite(media.GUID, login?.userID ?: "", currentUnixSeconds()))
-            }
-            isFav = !isFav
-        }
+        if (!isFav)
+            RequestQueue.addFav(media)
+        else
+            RequestQueue.removeFav(media)
+        isFav = media.isFav()
     }) {
         if (isFav)
             Icon(Icons.Filled.Star, "Fav")
