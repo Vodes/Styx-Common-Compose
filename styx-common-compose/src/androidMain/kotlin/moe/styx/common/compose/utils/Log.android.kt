e6e4b23a9f94da8d549cbe8d15ce79dde0394b09
Alex H <48185123+Vodes@users.noreply.github.com>
2024-02-26 20:15:49 +0100

    Use proper logcat logs on android

--- styx-common-compose/src/androidMain/kotlin/moe/styx/common/compose/utils/Log.android.kt
+++ styx-common-compose/src/androidMain/kotlin/moe/styx/common/compose/utils/Log.android.kt
@@ -1,5 +1,7 @@
 package moe.styx.common.compose.utils
 
+import android.util.Log as AndroidLog
+
 actual object Log : ALog() {
     override fun printMsg(message: String, prefix: String, source: String?, exception: Throwable?, printStack: Boolean) {
         val fallback = if (message.isBlank() && exception != null) "Exception: ${exception.message}" else message
@@ -9,7 +11,12 @@ actual object Log : ALog() {
         if (exception != null && message.isBlank())
             msg += "\n  Exception: ${exception.message}"
 
-        println(msg)
+        when (prefix) {
+            "I" -> AndroidLog.i(source, message, exception)
+            "D" -> AndroidLog.d(source, message, exception)
+            "W" -> AndroidLog.w(source, message, exception)
+            else -> AndroidLog.e(source, message, exception)
+        }
 
         if (printStack)
             exception?.printStackTrace()
