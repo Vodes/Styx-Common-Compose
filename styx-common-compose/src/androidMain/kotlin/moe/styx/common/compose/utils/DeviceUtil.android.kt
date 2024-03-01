2fb250ca65243b981a9dbb040ee265b04190e577
Alex H <48185123+Vodes@users.noreply.github.com>
2024-03-01 18:48:58 +0100

    fetchDeviceInfo: Check if tablet

--- styx-common-compose/src/androidMain/kotlin/moe/styx/common/compose/utils/DeviceUtil.android.kt
+++ styx-common-compose/src/androidMain/kotlin/moe/styx/common/compose/utils/DeviceUtil.android.kt
@@ -1,14 +1,16 @@
 package moe.styx.common.compose.utils
 
 import android.os.Build
+import com.russhwolf.settings.get
 import moe.styx.common.compose.appConfig
+import moe.styx.common.compose.settings
 import moe.styx.common.data.DeviceInfo
 
 actual fun fetchDeviceInfo(): DeviceInfo {
     val supportedABIs = Build.SUPPORTED_ABIS.joinToString()
     val supportSOC = Build.VERSION.SDK_INT >= 31
     return DeviceInfo(
-        "Phone",
+        if (settings["is-tablet", false]) "Tablet" else "Phone",
         "",
         "${Build.MANUFACTURER}/${Build.BRAND} ${Build.MODEL}",
         if (supportSOC) "${Build.SOC_MANUFACTURER} ${Build.SOC_MODEL} ($supportedABIs)" else "Unknown ($supportedABIs)",
--- styx-common-compose/src/iosMain/kotlin/moe/styx/common/compose/utils/DeviceUtil.ios.kt
+++ styx-common-compose/src/iosMain/kotlin/moe/styx/common/compose/utils/DeviceUtil.ios.kt
@@ -1,12 +1,14 @@
 package moe.styx.common.compose.utils
 
+import com.russhwolf.settings.get
 import moe.styx.common.compose.appConfig
+import moe.styx.common.compose.settings
 import moe.styx.common.data.DeviceInfo
 import platform.UIKit.UIDevice
 
 actual fun fetchDeviceInfo(): DeviceInfo {
     return DeviceInfo(
-        "Phone",
+        if (settings["is-tablet", false]) "Tablet" else "Phone",
         UIDevice.currentDevice.systemName(),
         null,
         null,
