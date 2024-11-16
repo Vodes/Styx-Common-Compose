a4836dd1b83322af6a76fa8b7721c4810231ee68
Alex H. <48185123+Vodes@users.noreply.github.com>
2024-11-16 02:58:16 +0100

    Bump to compose 1.7.1

--- styx-common-compose/build.gradle.kts
+++ styx-common-compose/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 group = "moe.styx"
-version = (System.getenv("SNAPSHOT_COMMIT") ?: "").ifBlank { "0.0.6-SNAPSHOT10" }
+version = (System.getenv("SNAPSHOT_COMMIT") ?: "").ifBlank { "0.1.0-SNAPSHOT1" }
 
 kotlin {
     applyDefaultHierarchyTemplate()
