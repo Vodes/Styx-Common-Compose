7d615472aa4acd4306852258914154b24754d92c
Alex H. <48185123+Vodes@users.noreply.github.com>
2024-12-17 12:37:35 +0100

    feat: MainScaffold overload allowing title composable

--- styx-common-compose/src/commonMain/kotlin/moe/styx/common/compose/components/layout/MainScaffold.kt
+++ styx-common-compose/src/commonMain/kotlin/moe/styx/common/compose/components/layout/MainScaffold.kt
@@ -6,6 +6,7 @@ import androidx.compose.animation.core.infiniteRepeatable
 import androidx.compose.animation.core.rememberInfiniteTransition
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.*
@@ -14,17 +15,17 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import moe.styx.common.compose.components.AppShapes
 import moe.styx.common.compose.components.buttons.PopButton
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScaffold(
     modifier: Modifier = Modifier,
     title: String,
     addPopButton: Boolean = true,
-    actions: @Composable () -> Unit = {},
+    actions: @Composable (RowScope.() -> Unit) = {},
     bottomBarContent: (@Composable () -> Unit)? = null,
     addAnimatedTitleBackground: Boolean = false,
     content: @Composable () -> Unit
@@ -36,23 +37,41 @@ fun MainScaffold(
         animationSpec = infiniteRepeatable(tween(3000), RepeatMode.Reverse),
         label = "title-color"
     )
+    MainScaffold(modifier, {
+        if (!addAnimatedTitleBackground)
+            Text(title, overflow = TextOverflow.Ellipsis, maxLines = 2)
+        else {
+            Surface(Modifier.clip(AppShapes.medium), shadowElevation = 1.dp, color = animatedColor) {
+                Text(title, Modifier.padding(5.dp), overflow = TextOverflow.Ellipsis, maxLines = 2)
+            }
+        }
+    }, addPopButton, actions, bottomBarContent, content = content)
+}
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainScaffold(
+    modifier: Modifier = Modifier,
+    titleContent: @Composable () -> Unit = {},
+    addPopButton: Boolean = true,
+    actions: @Composable (RowScope.() -> Unit) = {},
+    bottomBarContent: (@Composable () -> Unit)? = null,
+    topAppBarModifier: Modifier = Modifier,
+    topAppBarExpandedHeight: Dp = TopAppBarDefaults.TopAppBarExpandedHeight,
+    content: @Composable () -> Unit
+) {
     Scaffold(modifier = modifier, topBar = {
         TopAppBar(
             title = {
-                if (!addAnimatedTitleBackground)
-                    Text(title, overflow = TextOverflow.Ellipsis, maxLines = 2)
-                else {
-                    Surface(Modifier.clip(AppShapes.medium), shadowElevation = 1.dp, color = animatedColor) {
-                        Text(title, Modifier.padding(5.dp), overflow = TextOverflow.Ellipsis, maxLines = 2)
-                    }
-                }
+                titleContent()
             },
             actions = {
                 actions()
                 if (addPopButton)
                     PopButton()
-            }
+            },
+            expandedHeight = topAppBarExpandedHeight,
+            modifier = topAppBarModifier
         )
     }, bottomBar = { bottomBarContent?.let { it() } }) {
         Box(Modifier.fillMaxSize().padding(it)) {
