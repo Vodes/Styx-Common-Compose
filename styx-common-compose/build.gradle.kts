64ce4d360b360585d022d2ff2b53444400f4fca4
Alex H. <48185123+Vodes@users.noreply.github.com>
2025-05-30 01:25:22 +0200

    chore: Bump patch version

--- styx-common-compose/build.gradle.kts
+++ styx-common-compose/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 group = "moe.styx"
-version = (System.getenv("SNAPSHOT_COMMIT") ?: "").ifBlank { "0.2.1-SNAPSHOT1" }
+version = (System.getenv("SNAPSHOT_COMMIT") ?: "").ifBlank { "0.2.1" }
 
 kotlin {
     applyDefaultHierarchyTemplate()
