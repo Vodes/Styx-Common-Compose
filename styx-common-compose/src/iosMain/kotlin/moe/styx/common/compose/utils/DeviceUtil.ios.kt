ed1918a14c67c7cee3530f87020cc4e1c0bd9be4
Alex H. <48185123+Vodes@users.noreply.github.com>
2026-06-24 15:41:25 +0200

    fix: Less brittle iOS device info fetching

--- styx-common-compose/src/iosMain/kotlin/moe/styx/common/compose/utils/DeviceUtil.ios.kt
+++ styx-common-compose/src/iosMain/kotlin/moe/styx/common/compose/utils/DeviceUtil.ios.kt
@@ -1,51 +1,43 @@
 package moe.styx.common.compose.utils
 
 import com.russhwolf.settings.get
-import kotlinx.cinterop.ByteVar
 import kotlinx.cinterop.ExperimentalForeignApi
-import kotlinx.cinterop.ULongVar
 import kotlinx.cinterop.alloc
-import kotlinx.cinterop.allocArray
-import kotlinx.cinterop.convert
 import kotlinx.cinterop.memScoped
 import kotlinx.cinterop.ptr
 import kotlinx.cinterop.toKString
-import kotlinx.cinterop.value
 import moe.styx.common.compose.AppContextImpl.appConfig
 import moe.styx.common.compose.settings
 import moe.styx.common.data.DeviceInfo
 import moe.styx.common.util.Log
 import platform.Foundation.NSURL
-import platform.darwin.sysctlbyname
 import platform.UIKit.UIApplication
 import platform.UIKit.UIDevice
+import platform.posix.uname
+import platform.posix.utsname
 
 actual fun fetchDeviceInfo(): DeviceInfo {
-    val machine = sysctlString("hw.machine")
+    val machine = deviceMachineIdentifier()
 
     return DeviceInfo(
         if (settings["is-tablet", false]) "Tablet" else "Phone",
         UIDevice.currentDevice.name,
         UIDevice.currentDevice.model,
-        sysctlString("machdep.cpu.brand_string") ?: machine?.let { "Apple Silicon ($it)" },
+        machine?.let { "Apple Silicon ($it)" } ?: "Apple Silicon",
         null,
         UIDevice.currentDevice.systemName,
         UIDevice.currentDevice.systemVersion,
         null,
         null,
         appConfig().appSecret
-    ).also { Log.d { it.toString() } }
+    )
 }
 
 @OptIn(ExperimentalForeignApi::class)
-private fun sysctlString(name: String): String? = memScoped {
-    val size = alloc<ULongVar>()
-    if (sysctlbyname(name, null, size.ptr, null, 0.convert()) != 0) return null
-
-    val buffer = allocArray<ByteVar>(size.value.toInt())
-    if (sysctlbyname(name, buffer, size.ptr, null, 0.convert()) != 0) return null
-
-    buffer.toKString().takeIf { it.isNotBlank() }
+private fun deviceMachineIdentifier(): String? = memScoped {
+    val systemInfo = alloc<utsname>()
+    if (uname(systemInfo.ptr) != 0) return null
+    systemInfo.machine.toKString().takeIf { it.isNotBlank() }
 }
 
 actual fun openURI(uri: String?) {
